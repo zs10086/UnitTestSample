cf5a4988f541d0ee896cb5de623fdb420597565c
zs10086 <1074212278@qq.com>
2021-03-07 16:25:17 +0800

    添加项目文件。

---
+++ ClassLibrary1/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Math
+    {
+        public static bool IsOdd(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
