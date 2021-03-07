216bf854d797f18686fe569398d7ca301dd8de1b
zs10086 <1074212278@qq.com>
2021-03-07 19:43:11 +0800

    MathTest.IsOddTest提高测试覆盖率

--- ClassLibrary1.Tests/MathTest.cs
+++ ClassLibrary1.Tests/MathTest.cs
@@ -10,12 +10,15 @@ namespace ClassLibrary1.Tests
         {
             //Arrange
             var num = 1;
+            var num2 = 2;
 
             //Act
             bool result = Math.IsOdd(num);
+            bool result2 = Math.IsOdd(num2);
 
             //Assert
             Assert.True(result);
+            Assert.False(result2);
         }
     }
 }
