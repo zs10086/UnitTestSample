c059653014f391e310744612838a4ae54ddb855a
zs10086 <1074212278@qq.com>
2021-03-07 16:55:18 +0800

    添加Math类注释

--- ClassLibrary1/Math.cs
+++ ClassLibrary1/Math.cs
@@ -2,8 +2,16 @@
 
 namespace ClassLibrary1
 {
+    /// <summary>
+    /// 数学类
+    /// </summary>
     public class Math
     {
+        /// <summary>
+        /// 奇数判断
+        /// </summary>
+        /// <param name="n">待判断数字</param>
+        /// <returns></returns>
         public static bool IsOdd(int n)
         {
             if (n % 2 == 0)
